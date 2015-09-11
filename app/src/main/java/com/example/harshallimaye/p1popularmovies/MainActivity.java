0703dfc07edb82f1f77968d56f72bee3d6155a3c
harshallimaye <nhans2015!!>
2015-09-11 18:40:35 -0400

    Fixed a potential bug that could crash SettingsActivity.

--- app/src/main/java/com/example/harshallimaye/p1popularmovies/MainActivity.java
+++ app/src/main/java/com/example/harshallimaye/p1popularmovies/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,7 +14,7 @@ import android.view.View;
 import com.google.gson.Gson;
 
 
-public class MainActivity extends ActionBarActivity implements MainActivityFragment.Callback {
+public class MainActivity extends AppCompatActivity implements MainActivityFragment.Callback {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
